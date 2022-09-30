c0336bf4749118e46189d3beb43b2144cca9e8a1
Boglárka Száday <boglarka.szaday@stud.uni-corvinus.hu>
2022-09-30 14:30:47 +0200

    f08_utonev_kaloz

--- UserMaintenance/UserMaintenance/Resource.Designer.cs
+++ UserMaintenance/UserMaintenance/Resource.Designer.cs
@@ -70,7 +70,7 @@ namespace UserMaintenance {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Keresztnév.
+        ///   Looks up a localized string similar to Utonév.
         /// </summary>
         internal static string FirstName {
             get {
